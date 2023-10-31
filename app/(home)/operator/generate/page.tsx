8296328eac358779bc4816c57dca485dec66f689
salsabilatuadaa <salsabilatuadaa7@gmail.com>
2023-11-01 00:08:27 +0700

    halaman generate akun

--- app/(home)/operator/generate/page.tsx
+++ app/(home)/operator/generate/page.tsx
@@ -4,7 +4,6 @@ import GenerateForm from "@/components/GenerateForm";
 export default function Operator() {
   return (
     <div>
-      <h1>Generate Akun Mahasiswa</h1>
       <GenerateForm />
     </div>
   );
