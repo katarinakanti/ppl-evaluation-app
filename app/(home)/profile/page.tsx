83044014211bb301f4223717ec2f8f8ba7c31e55
salsabilatuadaa <salsabilatuadaa7@gmail.com>
2023-11-01 21:07:53 +0700

    rapihin profile

--- app/(home)/profile/page.tsx
+++ app/(home)/profile/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
           <h1 className="font-arial font-bold flex-row ml-60 mb-20 text-xl">Abigail Metanoia Melody</h1>
           <h4 className="font-arial">NiIM: 24060121120038</h4>
       </div> */}
-      <div className="flex mb-8 mt-16 mx-auto bg-gray-100 w-10/12 h-48">
+      <div className="flex mb-8 mt-10 mx-auto bg-gray-100 w-10/12 h-48">
         <div className="flex items-center justify-center ml-10">
           <img
             className="h-150 w-150 rounded-full"
@@ -36,93 +36,127 @@ export default async function Page() {
             width={150}
           />
         </div>
-        <div className="flex items-center ml-10">
-          <div>
-            <button>
-              <p className="text-xl mb-5 font-arial font-semibold text-left">
-                Abigail Metanoia Melody
-              </p>
-              <div className="font-medium  flex-1 justify-between">
-                <p className="text-left text-sm font-arial font-extralight">
-                  NIM : 240601211200038
-                </p>
-                <p className=" mt-5 flex-col text-left text-sm c font-extralight">
-                  Informatika
-                </p>
-              </div>
-            </button>
-          </div>
-        </div>
-      </div>
 
-      <div className="flex mb-8 mt-10 mx-auto bg-gray-100 w-10/12 h-96">
-        <div className="flex mx-auto bg-gray-300 w-screen h-10">
-          <p className="flex items-center ml-5 font-arial">Profil</p>
-          <div className="flex  flex-col items-center mt-20">
-            <img
-              src="/img/slide1.jpg"
-              alt="Foto_Mahasiswa"
-              height={150}
-              width={150}
-            />
-            <p className="mt-2 text-center text-xl font-arial font-medium">
-              2023/2024
-            </p>
-          </div>
-          <div className="flex flex-col items-center mt-20 ml-10">
-            <div className="text-left font-arial font-medium">
-              <table>
-                <tbody>
-                  <tr>
-                    <th>NIM</th>
-                    <td>24060121120038</td>
-                  </tr>
-                  <tr>
-                    <th>Nama Lengkap</th>
-                    <td>Abigail Metanoia Melody</td>
-                  </tr>
+        <div className="flex items-center justify-center ml-10">
+          <div className="flex-col w-full">
+            <p className="text-4xl mb-5 font-arial font-semibold text-left">Abigail Metanoia Melody</p>
 
-                  <tr>
-                    <th>Angkatan</th>
-                    <td>2021</td>
-                  </tr>
-                  <tr>
-                    <th>Jalur Masuk</th>
-                    <td>SBMPTN</td>
-                  </tr>
-                </tbody>
-              </table>
-              <table className="mt-10">
-                <tbody>
-                  <tr>
-                    <th>Nomor HP</th>
-                    <td>081994055237</td>
-                  </tr>
-                  <tr>
-                    <th>Email</th>
-                    <td>abigailmetanoia17@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <th>Alamat Asal</th>
-                    <td>Jl. KedungMundu Lama 189E KedungMundu RT 2 RW 5</td>
-                  </tr>
-                  <tr>
-                    <th>Kab/Kota</th>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <th>Propinsi</th>
-                    <td>Jawa Tengah</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div className="flex">
+              <p className="text-lg font-arial font-extralight">NIM : 240601211200038</p>
+              <p className="ml-32 text-lg font-arial font-extralight">Informatika</p>
             </div>
           </div>
-          <div className="flex flex-col mt-96 ml-96">
-            <button className="btn-primary">Simpan</button>
-          </div>
+        </div>
+        
+      </div>
+
+      <div className="flex mx-auto bg-gray-300 w-10/12 h-10">
+        <p className="flex items-center ml-5 font-arial">Profil</p>
+      </div>
+
+      <div className="mx-auto bg-gray-100 w-10/12 h-fit">
+
+        <div className="flex mx-auto pt-14">
+
+          <div className="flex items-center ml-24 justify-center">
+
+            <div className="flex-col items-center w-1/6 justify-center">
+              <img
+                src="/img/slide1.jpg"
+                alt="Foto_Mahasiswa"
+                height={150}
+                width={150}
+              />
+              <p className="mt-3 text-center text-xl font-arial font-medium">
+                2023/2024
+              </p>
+              <p className="mt-3 bg-lime-500 mx-auto w-2/3 rounded py-1 text-center text-medium font-medium">
+                Aktif
+              </p>            
+            </div>
+
+            <div className="flex flex-col items-center ml-14">
+              <div className="text-left font-arial font-medium">
+                <table className="w-full">
+                  <tbody>
+                    <tr>
+                      <th className="w-56">NIM</th>
+                      <td>24060121120038</td>
+                    </tr>
+                    <tr>
+                      <th>Nama Lengkap</th>
+                      <td>Abigail Metanoia Melody</td>
+                    </tr>
+                    <tr>
+                      <th>Angkatan</th>
+                      <td>2021</td>
+                    </tr>
+                    <tr>
+                      <th>Jalur Masuk</th>
+                      <td>SBMPTN</td>
+                    </tr>
+                    <tr>
+                      <th>Nomor HP</th>
+                      <td>081994055237</td>
+                    </tr>
+                    <tr>
+                      <th>Email</th>
+                      <td>abigailmetanoia17@gmail.com</td>
+                    </tr>
+                    <tr>
+                      <th>Alamat Asal</th>
+                      <td>Jl. KedungMundu Lama 189E KedungMundu RT 2 RW 5</td>
+                    </tr>
+                    <tr>
+                      <th>Kab/Kota</th>
+                      <td>Semarang</td>
+                    </tr>
+                    <tr>
+                      <th>Propinsi</th>
+                      <td>Jawa Tengah</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                {/* <table className="w-full">
+                  <tbody>
+                  </tbody>
+                </table> */}
+
+                </div>
+              </div>
+          </div> 
+        </div>
+
+        <div className="flex mt-10 justify-end mr-28">
+          <button className="bg-white hover:bg-blue-100 text-blue-400 border border-blue-400 px-7 py-1 font-semibold rounded mb-5">Simpan</button>
         </div>
       </div>
     </>
   );
 }
+
+
+{/* <div className="flex items-center ml-10">
+          <div>
+            <button>
+              <p className="text-xl mb-5 font-arial font-semibold text-left">
+                Abigail Metanoia Melody
+              </p>
+
+              <div className="flex flex-col">
+                <div className="flex w-full font-medium justify-between">
+                  <p className="text-sm font-arial font-extralight">
+                    NIM : 240601211200038
+                  </p>
+                </div>
+                <div>
+                  <p className="flex w-full mt-5 text-left text-sm c font-extralight">
+                    Informatika
+                  </p>
+                </div>
+              </div>
+
+            </button>
+          </div>
+        </div> */}
