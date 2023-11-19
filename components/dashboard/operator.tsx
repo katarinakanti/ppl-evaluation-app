2d1fa537187c057187cdfcafba0556fec8dcf31e
salsabilatuadaa <salsabilatuadaa7@gmail.com>
2023-11-20 02:45:07 +0700

    dashboard operator

--- components/dashboard/operator.tsx
+++ components/dashboard/operator.tsx
@@ -1,5 +1,6 @@
 import { Session } from "@supabase/supabase-js";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function OperatorDashboard({ session }: { session: Session }) {
   return (
@@ -29,6 +30,30 @@ export default function OperatorDashboard({ session }: { session: Session }) {
                 Informatika
               </p>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex mx-auto w-10/12">
+        <div className="mb-8 mt-10 mx-auto w-9/12 h-64">
+          <div className= "flex-col w-full">
+            <div className = "bg-gray-200 h-14 flex items-center ">
+              <p className="ml-10 text-lg">Status Akademik</p>
+            </div>
+            <div className = "bg-gray-100 h-48 flex items-center ">
+              <p className="ml-10 text-lg"></p>
+            </div>
+          </div>
+
+        </div>
+
+        <div className=" ml-10 mb-8 mt-10 mx-auto w-3/12 h-64">
+          <div className= "flex-col w-full"> 
+            <Link className = "bg-gray-100 h-12 flex items-center justify-center" href={"/operator/generate/mhs"}>Generate Mahasiswa</Link>
+            <Link className = "bg-gray-100 h-12 mt-5 flex items-center justify-center" href={"/operator/generate/dosen"}>Generate Dosen</Link>
+            <Link className = "bg-gray-100 h-12 mt-5 flex items-center justify-center" href={"/operator/generate/csv"}>Generate CSV</Link>
+            {/* <Link className = "bg-gray-100 h-10 mt-5 flex items-center justify-center" href={"/dosen/skripsi"}>Skripsi</Link> */}
+            {/* <Link className = "bg-gray-100 h-10 mt-3 flex items-center justify-center" href={"/dosen/search"}>Search Progress Studi</Link> */}
           </div>
         </div>
       </div>
